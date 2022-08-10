e894b3b26684a515605d0664fea38f218bf12b9e
zhou <1719351997@qq.com>
2022-08-10 15:33:54 +0800

    提交webGIS初步使用

---
+++ 11WEBGIS学习之路/vue-GIS/vue-gis/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="mapview"></div>
+</template>
+
+<script>
+import {loadModules} from "esri-loader";
+
+const options = {
+  url: "https://js.arcgis.com/4.23/init.js",
+  css: "https://js.arcgis.com/4.23/esri/themes/light/main.css",
+};
+
+export default {
+  name: "App",
+  components: {},
+  mounted() {
+    this.initMapView();
+  },
+  methods: {
+    async initMapView() {
+      const [Map, MapView] = await loadModules(
+        ["esri/Map", "esri/views/MapView"],
+        options
+      );
+      const map = new Map({
+        basemap: "osm",
+      });
+      const view = new MapView({
+        container: "mapview",
+        map: map,
+        zoom: 4,
+        center: [104.064857, 30.580685],
+      });
+      console.log(view);
+    },
+  },
+};
+</script>
+
+<style>
+html,
+body,
+#mapview {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+}
+</style>
---
+++ 11WEBGIS学习之路/vue-GIS/vue-gis/src/components/demo.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import {esri} from 'esri-loader';
+
+const options = {
+  url: "https://js.arcgis.com/4.23/init.js",
+  css: "https://js.arcgis.com/4.23/esri/themes/light/main.css",
+};
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted() {
+    this.initMapView()
+  },
+  methods: {
+    async initMapView() {
+      const [Map, MapView] = await esri(
+        ["esri/Map", "esri/views/MapView"],
+        options
+      );
+      const map = new Map({
+        basemap: "osm",
+      });
+      const view = new MapView({
+        container: "mapview",
+        map: map,
+        zoom: 4,
+        center: [104.064857, 30.580685],
+      });
+      console.log(view);
+    },
+  },
+
+}
+</script>
+
