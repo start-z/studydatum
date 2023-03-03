3bdb1f974532a0c308de60d89b1f5a45677df2d8
zhou <1719351997@qq.com>
2023-03-03 09:55:34 +0800

    流媒体接口代码优化

--- 10其他功能点/src/main/java/com/zhou/controller/Mp4Controller.java
+++ 10其他功能点/src/main/java/com/zhou/controller/Mp4Controller.java
@@ -1,5 +1,6 @@
 package com.zhou.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -19,6 +20,7 @@ import java.io.*;
  */
 @Controller
 @RequestMapping("/profile")
+@Slf4j
 public class Mp4Controller {
 
     @GetMapping("/toMp4ByFileSteam1/{path}")
@@ -53,61 +55,69 @@ public class Mp4Controller {
                 String rangBytes = "";
                 fileLength = file.length();
 
-                // get file content
+                // 得到文件的长度
                 InputStream ins = new FileInputStream(file);
                 bis = new BufferedInputStream(ins);
 
-                // tell the client to allow accept-ranges
+                //告诉bs端进行断点续传
                 response.reset();
                 response.setHeader("Accept-Ranges", "bytes");
 
-                // client requests a file block download start byte
+                // 客户端下载指定字节
                 String range = request.getHeader("Range");
                 if (range != null && range.trim().length() > 0 && !"null".equals(range)) {
                     response.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);
                     rangBytes = range.replaceAll("bytes=", "");
-                    if (rangBytes.endsWith("-")) { // bytes=270000-
+                    // 得到rangge请求位置然后判断读取位置
+                    if (rangBytes.endsWith("-")) {
                         rangeSwitch = 1;
                         p = Long.parseLong(rangBytes.substring(0, rangBytes.indexOf("-")));
-                        contentLength = fileLength - p; // 客户端请求的是270000之后的字节（包括bytes下标索引为270000的字节）
-                    } else { // bytes=270000-320000
+                        contentLength = fileLength - p;
+                    } else {
                         rangeSwitch = 2;
                         String temp1 = rangBytes.substring(0, rangBytes.indexOf("-"));
-                        String temp2 = rangBytes.substring(rangBytes.indexOf("-") + 1, rangBytes.length());
+                        String temp2 = rangBytes.substring(rangBytes.indexOf("-") + 1);
                         p = Long.parseLong(temp1);
                         toLength = Long.parseLong(temp2);
-                        contentLength = toLength - p + 1; // 客户端请求的是 270000-320000 之间的字节
+                        contentLength = toLength - p + 1;
                     }
                 } else {
                     contentLength = fileLength;
                 }
 
-                // 如果设设置了Content-Length，则客户端会自动进行多线程下载。如果不希望支持多线程，则不要设置这个参数。
+                // 如果设置了Content-Length，则客户端会自动进行多线程下载。如果不希望支持多线程，则不要设置这个参数。
                 // Content-Length: [文件的总大小] - [客户端请求的下载的文件块的开始字节]
-                response.setHeader("Content-Length", new Long(contentLength).toString());
+                response.setHeader("Content-Length", Long.toString(contentLength));
 
                 // 断点开始
                 // 响应的格式是:
                 // Content-Range: bytes [文件块的开始字节]-[文件的总大小 - 1]/[文件的总大小]
                 if (rangeSwitch == 1) {
-                    String contentRange = new StringBuffer("bytes ").append(new Long(p).toString()).append("-")
-                            .append(new Long(fileLength - 1).toString()).append("/")
-                            .append(new Long(fileLength).toString()).toString();
+                    String contentRange = new StringBuffer("bytes ")
+                            .append(p)
+                            .append("-")
+                            .append(fileLength - 1)
+                            .append("/")
+                            .append(fileLength)
+                            .toString();
                     response.setHeader("Content-Range", contentRange);
-                    bis.skip(p);
+                    long skip = bis.skip(p);
                 } else if (rangeSwitch == 2) {
-                    String contentRange = range.replace("=", " ") + "/" + new Long(fileLength).toString();
+                    String contentRange = range.replace("=", " ") + "/" + fileLength;
                     response.setHeader("Content-Range", contentRange);
-                    bis.skip(p);
+                    long skip = bis.skip(p);
                 } else {
-                    String contentRange = new StringBuffer("bytes ").append("0-").append(fileLength - 1).append("/")
-                            .append(fileLength).toString();
+                    String contentRange = new StringBuffer("bytes ")
+                            .append("0-")
+                            .append(fileLength - 1)
+                            .append("/")
+                            .append(fileLength)
+                            .toString();
                     response.setHeader("Content-Range", contentRange);
                 }
 
                 String fileName = file.getName();
                 response.setContentType("application/octet-stream");
-//                不需要下载流
                 response.addHeader("Content-Disposition", "attachment;filename=" + fileName);
 
                 OutputStream out = response.getOutputStream();
@@ -116,7 +126,6 @@ public class Mp4Controller {
                 int bsize = 1024;
                 byte[] bytes = new byte[bsize];
                 if (rangeSwitch == 2) {
-                    // 针对 bytes=27000-39000 的请求，从27000开始写数据
                     while (readLength <= contentLength - bsize) {
                         n = bis.read(bytes);
                         readLength += n;
@@ -135,10 +144,8 @@ public class Mp4Controller {
                 out.close();
                 bis.close();
             }
-        } catch (IOException ie) {
-            // 忽略 ClientAbortException 之类的异常
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("流媒体播放异常,{0}", e);
         }
     }
 }
