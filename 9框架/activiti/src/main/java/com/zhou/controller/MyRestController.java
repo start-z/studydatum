b994d55e2fe11257d23e631b8104d1d155053bb7
zhou <1719351997@qq.com>
2023-04-14 17:34:40 +0800

    activiti二次学习提交

--- 9框架/activiti/src/main/java/com/zhou/controller/MyRestController.java
+++ 9框架/activiti/src/main/java/com/zhou/controller/MyRestController.java
@@ -13,14 +13,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class MyRestController {
@@ -28,15 +30,21 @@ public class MyRestController {
     @Autowired
     private MyService myService;
 
+    @Resource
+    TaskService taskService;
 
-    @RequestMapping(value = "/creat", method = RequestMethod.GET)
+
+    /**
+     * 部署流程
+     */
+    @RequestMapping(value = "/deploy", method = RequestMethod.GET)
     public void init() {
         //部署流程
         ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
         RepositoryService repositoryService = processEngine.getRepositoryService();
         Deployment deploy = repositoryService
                 .createDeployment()
-                .addClasspathResource("processes/test.bpmn20.xml")
+                .addClasspathResource("processes/VacationRequest.bpmn20.xml")
                 .deploy();
         System.out.println(deploy + " 创建成功");
     }
@@ -50,17 +58,46 @@ public class MyRestController {
     /**
      * 查看当前任务流程
      *
-     * @param assignee 流程key
      * @return 任务列表
      */
     @RequestMapping(value = "/tasks", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<TaskRepresentation> getTasks(@RequestParam String assignee) {
-        List<Task> tasks = myService.getTasks(assignee);
+    public List<TaskRepresentation> getTasks() {
+        List<Task> tasks = myService.getTasks();
         List<TaskRepresentation> dtos = new ArrayList<TaskRepresentation>();
         for (Task task : tasks) {
             dtos.add(new TaskRepresentation(task.getId(), task.getName()));
         }
         return dtos;
+    }
+
+    /**
+     * 查看当前任务流程
+     *
+     * @return 任务列表
+     */
+    @RequestMapping(value = "/feachTasks", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Object feachTasks() {
+        List<Task> list = taskService.createTaskQuery().taskCandidateGroup("management").list();
+        List<Object> dtos = new ArrayList<Object>();
+        for (Task task : list) {
+            dtos.add(task);
+        }
+        return dtos;
+    }
+
+    /**
+     * 完成当前任务
+     *
+     * @return 任务列表
+     */
+    @RequestMapping(value = "/complete", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public void complete() {
+        Task task = taskService.createTaskQuery().taskCandidateGroup("management").list().get(0);
+        System.out.println(task);
+        Map<String, Object> taskVariables = new HashMap<String, Object>();
+        taskVariables.put("vacationApproved", "false");
+        taskVariables.put("managerMotivation", "We have a tight deadline!");
+        taskService.complete(task.getId(), taskVariables);
     }
 
     /**
@@ -72,7 +109,7 @@ public class MyRestController {
     public void getProcessImg(HttpServletResponse response) {
         RepositoryService repositoryService = ProcessEngines.getDefaultProcessEngine().getRepositoryService();
         ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery()
-                .processDefinitionKey("test")
+                .processDefinitionKey("vacationRequest")
                 .latestVersion()
                 .singleResult();
 
@@ -85,15 +122,6 @@ public class MyRestController {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    /**
-     * 完成任务
-     */
-    @RequestMapping(method = RequestMethod.GET, value = "finishTask")
-    public void finishTask() {
-        TaskService taskService = ProcessEngines.getDefaultProcessEngine().getTaskService();
-        taskService.complete("2509");
     }
 
     static class TaskRepresentation {
--- 9框架/activiti/src/main/java/com/zhou/service/MyService.java
+++ 9框架/activiti/src/main/java/com/zhou/service/MyService.java
@@ -1,36 +1,47 @@
 package com.zhou.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author BJB314
  */
 @Service
+@Slf4j
 public class MyService {
 
-   @Resource
+    @Resource
     private RuntimeService runtimeService;
 
     @Resource
     private TaskService taskService;
-    public void startProcess(String demo) {
-        ProcessInstance oneTaskProcess = runtimeService.startProcessInstanceByKey("test");
-        System.out.println("流程定义id：" + oneTaskProcess.getProcessDefinitionId());
-        System.out.println("流程实例id：" + oneTaskProcess.getId());
-        System.out.println("当前活动Id：" + oneTaskProcess.getActivityId());
+
+    public void startProcess() {
+        //设置流程变量
+        Map<String, Object> variables = new HashMap<String, Object>();
+        variables.put("employeeName", "Kermit");
+        variables.put("numberOfDays", new Integer(4));
+        variables.put("vacationMotivation", "I'm really tired!");
+        //key为xml中定义的id
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("vacationRequest", variables);
+        log.info("Number of process instances: " + runtimeService.createProcessInstanceQuery().count());
+        System.out.println("流程定义id：" + processInstance.getProcessDefinitionId());
+        System.out.println("流程实例id：" + processInstance.getId());
+        System.out.println("当前活动Id：" + processInstance.getActivityId());
     }
 
-    public List<Task> getTasks(String assignee) {
+    public List<Task> getTasks() {
         return taskService.createTaskQuery()
-                .processDefinitionKey("test")
+                .processDefinitionKey("vacationRequest")
 //                .taskAssignee(assignee)
                 .list();
     }
