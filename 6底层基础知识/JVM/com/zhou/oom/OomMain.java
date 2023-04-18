1a1fd08528155d93ea3c6a6926e8a52686e7f241
zhou <1719351997@qq.com>
2023-04-18 10:59:09 +0800

    JVM OOMprofiler工具使用

---
+++ 6底层基础知识/JVM/com/zhou/oom/OomMain.java
@@ -0,0 +1,22 @@
+package zhou.oom;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author zhou
+ * @since 2023/4/18
+ * description:  模拟内存溢出并使用性能分析软件排查问题
+ */
+public class OomMain {
+    //   jvm设置参数 -Xmx10M -XX:MaxDirectMemorySize=10M -Xloggc:gc.log
+    private static final int _10MB = 10 * 1024 * 1024;
+
+    public static void main(String[] args) throws Exception {
+        List<ByteBuffer> list = new ArrayList<>();
+        // 分配 20MB
+        list.add(ByteBuffer.allocateDirect(_10MB));
+        list.add(ByteBuffer.allocateDirect(_10MB));
+    }
+}
