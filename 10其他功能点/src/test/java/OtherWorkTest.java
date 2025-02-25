04ca2858a4ce2a75406ef765e30e709263527c71
zhou <@iii>
2025-02-25 08:49:59 +0800

    修正注解

--- 10其他功能点/src/test/java/OtherWorkTest.java
+++ 10其他功能点/src/test/java/OtherWorkTest.java
@@ -1,7 +1,7 @@
 import com.zhou.othWorkApplication;
 import com.zhou.service.OtherWorkService;
 import com.zhou.utils.ReOfferA;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
